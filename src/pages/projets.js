24a4021ca2a576f264dd7e285049e223445fe0d0
bhenbe <contact@bhen.be>
2020-04-06 22:04:24 +0200

    add pages

---
+++ src/pages/projets.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ProjectsPage = () => (
+  <Layout>
+    <SEO title="Projets" />
+    <h1>Projets</h1>
+  </Layout>
+)
+
+export default ProjectsPage
---
+++ src/pages/services.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ServicesPage = () => (
+  <Layout>
+    <SEO title="Services" />
+    <h1>Services</h1>
+  </Layout>
+)
+
+export default ServicesPage
